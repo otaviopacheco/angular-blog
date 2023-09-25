35f6881032d1f2d92fbae93e11f6743b2da05a3b
otaviopacheco <aapo5719@gmail.com>
2023-09-25 19:38:41 -0300

    styles updates

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+  photoCover:string = ""
+  cardDescription:string = ""
+  constructor(){}
+  ngOnInit(): void {
+
+  }
+
 
 }
